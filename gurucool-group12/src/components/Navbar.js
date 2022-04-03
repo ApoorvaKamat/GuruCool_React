7285fb25f7e2734e1befe19707b9573213c1e826
Apoorva Kamat <apoorvaKamat885@outlook.com>
2022-04-02 19:39:14 -0500

    Adding Dropdown Components

--- gurucool-group12/src/components/Navbar.js
+++ gurucool-group12/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Button from './buttonLogin';
 import Option from './option';
+import { NavBarDropdown } from './navBarDropdown';
+
+
 const Navbar = () => {
     return (
         <div >
             <nav className="navbar navbar-expand-sm bg-info navbar-dark bg-dark ps-5 pe-5 navblack">
+            <NavBarDropdown />
             <Option symbol="p-2 bi bi-person" option="Know Your Tutors"/>
             <Option symbol="p-2 bi bi-calendar-check"option="Book An Appointment"/>
             <Button name="Sign Up"/>
-            <Button name="Login"/>     
+            <Button name="Login"/>                        
             </nav>
         </div>
         
