3f1c4d233ff9b14b6b2dc44e660c7a3b6eb1ec28
Apoorva Kamat <apoorvaKamat885@outlook.com>
2022-04-02 20:05:35 -0500

    Update buttonLogin.js

--- gurucool-group12/src/components/buttonLogin.js
+++ gurucool-group12/src/components/buttonLogin.js
@@ -2,7 +2,7 @@ import React from "react";
 const Button=(props)=>{
     return(
         <div>
-            <button class=" btn btn-light btn-md ms-2 loginSignUp">{props.name} </button>
+            <button className=" btn btn-light btn-md ms-2 loginSignUp">{props.name} </button>
         </div>
     );
 };
