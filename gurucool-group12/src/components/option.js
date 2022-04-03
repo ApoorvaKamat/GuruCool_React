6787d10acce1479da6e47b65a4fe8d30f6500bf4
Apoorva Kamat <apoorvaKamat885@outlook.com>
2022-04-02 20:04:25 -0500

    Update option.js
    Changing Class to ClassName

--- gurucool-group12/src/components/option.js
+++ gurucool-group12/src/components/option.js
@@ -2,7 +2,7 @@ import React from "react";
 const Option=(props)=>{
     return(
         <div>
-            <div class="collapse navbar-collapse justify-content-end pe-3">
+            <div className="collapse navbar-collapse justify-content-end pe-3">
                      
                      <a href="#" className="text-white p-4 nav-item nav-link navElementAdjust"> 
                          <i className={props.symbol}></i>{props.option}</a>
