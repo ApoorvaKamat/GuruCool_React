48b878492569706a2e85854d3c77b291c4a15775
09Shambhavi <68826634+09Shambhavi@users.noreply.github.com>
2022-05-10 18:48:50 -0500

    2 features added
    Feedback done
    Booking date and time done

--- gurucool-group12/src/components/buttonLogin.js
+++ gurucool-group12/src/components/buttonLogin.js
@@ -2,7 +2,7 @@ import React from "react";
 const Button=(props)=>{
     return(
         <div>
-            <button className={`btn btn-light btn-md ms-2 loginSignUp ${props.cls?props.cls:''} `}>{props.name} </button>
+            <button  className={`btn btn-light btn-md ms-2 loginSignUp ${props.cls?props.cls:''} `}>{props.name} </button>
         </div>
     );
 };
--- gurucool-group12/src/components/cards.js
+++ gurucool-group12/src/components/cards.js
@@ -1,45 +1,70 @@
 import React,{useState} from "react";
-import { MDBBtn,
-  MDBModal,
-  MDBModalDialog,
-  MDBModalContent,
-  MDBModalHeader,
-  MDBModalTitle,
-  MDBModalBody,
-  MDBModalFooter,
-} from 'mdb-react-ui-kit';
+import { MDBBtn,MDBModal,MDBModalDialog,MDBModalContent,MDBModalHeader,MDBModalTitle,MDBModalBody,MDBModalFooter} from 'mdb-react-ui-kit';
 import defaultProfilePic from "../assets/default-profile.svg"
 import Button from './buttonLogin';
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+
+import Stars from './starRating'
+import TextField from "@material-ui/core/TextField";
+const starRating = {
+  size: 30,
+  count: 5,
+  half: true,
+  onChange: newValue => {
+    console.log(`Rating is ${newValue}`)
+  }
+}
 
 
 export const Cards = (props) => {
   const [selectedTutor, setSelectedTutor]= useState({});
-  const onTutorSelect = (tutor) => {
-    setCentredModal(!centredModal)
-    setSelectedTutor(tutor);
-  }
-  const [centredModal, setCentredModal] = useState(false);
+ 
+const [buttonName, setButtonName] = useState('');
+const [centredModal, setCentredModal] = useState(false);
+const [inputValue, setInputValue] = useState("");
+const handleUserInput = (e) => {
+  setInputValue(e.target.value);
+};
+const resetInputField = () => {
+  setInputValue("");
+  setCentredModal(!centredModal);
+};
+const toggleShow = () => {
+  setCentredModal(!centredModal);
+  setInputValue("");
+}
+const onButtonClick = (buttonName,tutor) => {
+ setButtonName(buttonName);
+ setCentredModal(!centredModal);
 
-  const toggleShow = () => setCentredModal(!centredModal);
-
-
-  return(
-        <>
-            {props.filterData && props.filterData.map((tutor,index)=>{
-            const {tutorName,tutorExpertise, profilePicture, tutorDescription, tutorLocation, tutorRating, workingHours} = tutor;
-            return <div key={`tutor-${index}`} onClick={()=> onTutorSelect(tutor)} className="bg-light opacity-75 tutor-card-container" >
-                <div className="tutor-name">{tutorName}<span className="favorite"><i class="bi bi-heart-fill"></i></span></div>
-                
-                <div className="tutor-profile">
-                  <div className="profile-pic-container">
-                     <img src={profilePicture?profilePicture:defaultProfilePic} alt={defaultProfilePic} className="profile-pic"></img>
-                  </div>
+ setSelectedTutor(tutor);
+}
+const [startDate, setStartDate] = useState(
+  setHours(setMinutes(new Date(), 0), 17)
+);
+return(
+<>
+{
+  props.filterData && props.filterData.map(
+  (tutor,index)=>
+  {
+  const {tutorName,tutorExpertise, profilePicture, tutorDescription, tutorLocation, tutorRating, workingHours} = tutor;
+  return <div key={`tutor-${index}`}  className="bg-light opacity-75 tutor-card-container" >
+  <div className="tutor-name">{tutorName}<span className="favorite"><i class="bi bi-heart-fill"></i></span></div>
+  {/* onClick={()=> onTutorSelect(tutor)} */}
+  <div className="tutor-profile">
+  <div className="profile-pic-container">
+  <img src={profilePicture?profilePicture:defaultProfilePic} alt={defaultProfilePic} className="profile-pic"></img>
+  </div>
                   <div className="profile-details">
                   <div className="subject">
                   <span><i class="bi bi-trophy-fill"></i> {tutorExpertise}</span>
                 </div>
                 <div className="rating">
-                 {/* how to add stars dynamically based on ratings? */}
                     <span className="star"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></span>
                       <span> {tutorRating}</span>
                 </div>
@@ -65,45 +90,120 @@ export const Cards = (props) => {
                   </div>
                 </div>
                 <div>
-                    <Button name="PROFILE" cls="button-card"/><Button cls="button-card" name="RATE"/><Button cls="button-card" name="BOOK APPOINTMENT"/>
+                  
+                    <button  onClick={()=>onButtonClick('profile',tutor) }>Profile</button>
+                    <button  onClick={()=>onButtonClick('rating',tutor)}>Rating</button>
+                    <button onClick={()=>onButtonClick('booking',tutor)}>Booking</button>
+                    <div>     
+              {
+                (() => {
+                    if(buttonName==='profile') {
+                            return (
+                            
+                              <MDBModal  tabIndex='-1' show={centredModal} setShow={setCentredModal}>
+                              <MDBModalDialog >
+                                <MDBModalContent>
+                                  <MDBModalHeader>
+                                    <MDBModalTitle>{selectedTutor.tutorName}</MDBModalTitle>
+                                    <button class="btn btn-danger btn-md " onClick={toggleShow}>Close</button>
+                                  </MDBModalHeader>
+                                  <MDBModalBody >
+                                 <div>
+                                  <span className="bold-font"> Know your tutor : </span>
+                                  <span>{selectedTutor.aboutTutor}</span>
+                                 </div>
+                                <div>
+                                  <span className="bold-font"> No.of working hours : </span>
+                                  <span>{selectedTutor.noOfHrsPerDay}</span>
+                                </div>
+                                <div>
+                                  <span className="bold-font"> No.of available appointments : </span>
+                                  <span>{selectedTutor.noOfAppointmentsAvailable}</span>
+                                </div>
+                                
+                                  </MDBModalBody>
+                                
+                                </MDBModalContent>
+                              </MDBModalDialog>
+                            </MDBModal>
+                            )
+                        } else if (buttonName==='rating') {
+                            return (
+                              <MDBModal  tabIndex='-1' show={centredModal} setShow={setCentredModal}>
+                              <MDBModalDialog >
+                                <MDBModalContent>
+                                  <MDBModalHeader>
+                                    <MDBModalTitle>{selectedTutor.tutorName}</MDBModalTitle>
+                                    <div class='d-flex justify-content-center'>
+                                    
+                                    <button class="btn btn-success mr-2" onClick={resetInputField}>Submit</button>
+                                    <button class="btn btn-danger btn-md " onClick={toggleShow}>Close</button>
+                                    </div>
+                                  </MDBModalHeader>
+                                  <MDBModalBody >
+                              <div>
+                              <span className="bold-font" > Share your feedback </span>
+                                <TextField  color="primary" focused value={inputValue} onChange={handleUserInput}/>
+                                <Stars {...starRating} />
+                                
+                            </div>
+                            </MDBModalBody>
+                                 
+                                </MDBModalContent>
+                              </MDBModalDialog>
+                            </MDBModal>
+                          
+          
+                            )
+                        } else {
+                            return (
+                              <MDBModal class="modal" tabIndex='-1' show={centredModal} setShow={setCentredModal}>
+                              <MDBModalDialog >
+                                <MDBModalContent className="modalcontent">
+                                  <MDBModalHeader>
+                                    <MDBModalTitle>{selectedTutor.tutorName}</MDBModalTitle>
+                                    
+                                    {/* <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn> */}
+                                    <div class='d-flex justify-content-center'>
+                                    <button class="btn btn-success mr-2" onClick={resetInputField}>Book</button>
+                                    <button class="btn btn-danger btn-md " onClick={toggleShow}>Cancel</button>
+                                    </div>
+                                  </MDBModalHeader>
+                                  <MDBModalBody >
+                                  <DatePicker className="date-picker-wrapper"
+                                    selected={startDate}
+                                    onChange={(date) => setStartDate(date)}
+                                    showTimeSelect
+                                    minTime={setHours(setMinutes(new Date(), 0), 9)}
+                                    maxTime={setHours(setMinutes(new Date(), 0), 19)}
+                                    dateFormat="MMMM d, yyyy h:mm aa"
+                                  />
+                                 
+                                 
+                                  </MDBModalBody>
+                                  <MDBModalFooter>   
+                                  </MDBModalFooter>
+                                </MDBModalContent>
+                              </MDBModalDialog>
+                            </MDBModal>
+                            )
+                        }
+                })()  
+            }  
+        </div>  
+                     
                     </div>
-            </div>
-        
+                 
             
-          })
-        
+                    </div>
+                 
           }
-      <MDBModal  tabIndex='-1' show={centredModal} setShow={setCentredModal}>
-        <MDBModalDialog  >
-          <MDBModalContent>
-            <MDBModalHeader>
-              <MDBModalTitle>{selectedTutor.tutorName}</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-            </MDBModalHeader>
-            <MDBModalBody >
-           <div>
-           <span className="bold-font"> Know your tutor : </span>
-                <span>{selectedTutor.aboutTutor}</span>
-
-           </div>
-              <div>
-              <span className="bold-font"> No.of working hours : </span>
-                <span>{selectedTutor.noOfHrsPerDay}</span>
-              </div>
-            <div>
-              <span className="bold-font"> No.of available appointments : </span>
-                <span>{selectedTutor.noOfAppointmentsAvailable}</span>
-              </div>
-
-              
-             
-            </MDBModalBody>
-            <MDBModalFooter>
-             
-            </MDBModalFooter>
-          </MDBModalContent>
-        </MDBModalDialog>
-      </MDBModal>
+        )
+        
+        }
+        
+      
+      
         </>
 
         
